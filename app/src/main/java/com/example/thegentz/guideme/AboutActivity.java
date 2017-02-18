de6a3b9141ca4bfe05491a277d4b075b5888ef96
Mushfique Ahmed <ahmed.mush@ymail.com>
2017-02-18 00:49:46 -0600

    Deleted unnecessary imports

--- app/src/main/java/com/example/thegentz/guideme/AboutActivity.java
+++ app/src/main/java/com/example/thegentz/guideme/AboutActivity.java
@@ -17,5 +17,9 @@ import android.widget.Toast;
 
 
 public class AboutActivity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.about);
 
 }
