d1cf481932e6adbad31ee26256f64ea05e995782
Mushfique Ahmed <ahmed.mush@ymail.com>
2017-02-18 00:46:07 -0600

    Added button which upon click will display menu and also created java class and xml file for About

---
+++ app/src/main/java/com/example/thegentz/guideme/AboutActivity.java
@@ -0,0 +1,21 @@
+package com.example.thegentz.guideme;
+import android.app.Activity;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.PopupMenu;
+import android.view.MenuInflater;
+import android.view.Menu;
+import android.view.ViewGroup;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.PopupMenu.OnMenuItemClickListener;
+import android.widget.Toast;
+
+
+public class AboutActivity extends Activity {
+
+}
