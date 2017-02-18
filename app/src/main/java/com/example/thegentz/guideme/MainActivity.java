2efe32097a5dac5fb292b521cc5959cb3ae76cb7
Mushfique Ahmed <ahmed.mush@ymail.com>
2017-02-18 00:28:40 -0600

    Added menu.xml

--- app/src/main/java/com/example/thegentz/guideme/MainActivity.java
+++ app/src/main/java/com/example/thegentz/guideme/MainActivity.java
@@ -1,42 +1,67 @@
 package com.example.thegentz.guideme;
-
+import android.app.Activity;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.PopupMenu;
 import android.view.MenuInflater;
-import android.R;
+
 import android.view.Menu;
 import android.view.ViewGroup;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.PopupMenu.OnMenuItemClickListener;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
 
-    // Used to load the 'native-lib' library on application startup.
-    static {
-        System.loadLibrary("native-lib");
-    }
+public class MainActivity extends Activity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Example of a call to a native method
-        TextView tv = (TextView) findViewById(R.id.sample_text);
-        tv.setText(stringFromJNI());
-    }
-    public void showPopup(View v) {
-        PopupMenu popup = new PopupMenu(this, v);
-        MenuInflater inflater = popup.getMenuInflater();
-        inflater.inflate(R.menu.main, popup.getMenu());
-        popup.show();
+        Button btn = (Button) findViewById(R.id.btn);
+
+        OnClickListener listener = new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                /** Instantiating PopupMenu class */
+                PopupMenu popup = new PopupMenu(getBaseContext(), v);
+
+                /** Adding menu items to the popumenu */
+                popup.getMenuInflater().inflate(R.menu.main, popup.getMenu());
+
+                /** Defining menu item click listener for the popup menu */
+                popup.setOnMenuItemClickListener(new OnMenuItemClickListener() {
+
+                    @Override
+                    public boolean onMenuItemClick(MenuItem item) {
+                        Toast.makeText(getBaseContext(), "You selected the action : " + item.getTitle(), Toast.LENGTH_SHORT).show();
+                        return true;
+                    }
+                });
+
+                /** Showing the popup menu */
+                popup.show();
+
+            }
+        };
+
+        btn.setOnClickListener(listener);
     }
 
-    /**
-     * A native method that is implemented by the 'native-lib' native library,
-     * which is packaged with this application.
-     */
-    public native String stringFromJNI();
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
 }
